f2f76cf5982e4b5b08dbe0a1def2c31d571abd44
guo.xin <xin1.guo@changhong.com>
2019-03-18 13:28:42 +0800

    user

--- user/src/main/java/com/example/user/users/usernumber.java
+++ user/src/main/java/com/example/user/users/usernumber.java
@@ -3,7 +3,8 @@ package com.example.user.users;
 import com.example.number.gx.userinfo;
 import org.springframework.stereotype.Service;
 
-@Service("1.0.0")
+
+@Service
 public class usernumber implements userinfo {
 
     @Override
